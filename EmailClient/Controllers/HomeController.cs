86669d0e8522b3301529f235f03395fb34d0caa3
Svetlin Nikolov <daxteranikolov@gmail.com>
2025-04-10 21:45:11 +0300

    Add some css styling

---
+++ EmailClient/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailClient.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[Route("/")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
